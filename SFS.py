ed5312308fc7605beb12f3393b450b633693e7a6
mhpandya <mhpandya@ncsu.edu>
2017-04-09 22:18:19 -0400

    Add files via upload

--- SFS.py
+++ SFS.py
@@ -1,6 +1,18 @@
 from sklearn.neighbors import KNeighborsClassifier
 from pandas import read_csv
 from mlxtend.feature_selection import SequentialFeatureSelector as SFS
+import sys
+
+# The name of the algorithm should be given as input
+if len(sys.argv) != 2:
+    print('SFS.py < SFS SBS SFFS SBFS >')
+    sys.exit(1)
+
+function = sys.argv[1]
+# check if input is correct
+if function not in ['SFS', 'SBS', 'SFFS', 'SBFS']:
+	print('SFS.py < SFS SBS SFFS SBFS >')
+	sys.exit(1)
 
 url = "MDP_Original_data2.csv"
 names = ['student', 'currProb', 'course', 'session', 'priorTutorAction', 'reward', 'Interaction', 'hintCount', 'TotalTime', 'TotalPSTime', 'TotalWETime', 'avgstepTime', 'avgstepTimePS', 'stepTimeDeviation', 'symbolicRepresentationCount', 'englishSymbolicSwitchCount', 'Level', 'probDiff', 'difficultProblemCountSolved', 'difficultProblemCountWE', 'easyProblemCountSolved', 'easyProblemCountWE', 'probAlternate', 'easyProbAlternate', 'RuleTypesCount', 'UseCount', 'PrepCount', 'MorphCount', 'OptionalCount', 'NewLevel', 'SolvedPSInLevel', 'SeenWEinLevel', 'probIndexinLevel', 'probIndexPSinLevel', 'InterfaceErrorCount', 'RightApp', 'WrongApp', 'WrongSemanticsApp', 'WrongSyntaxApp', 'PrightAppRatio', 'RrightAppRatio', 'F1Score', 'FDActionCount', 'BDActionCount', 'DirectProofActionCount', 'InDirectProofActionCount', 'actionCount', 'UseWindowInfo', 'NonPSelements', 'AppCount', 'AppRatio', 'hintRatio', 'BlankRatio', 'HoverHintCount', 'SystemInfoHintCount', 'NextStepClickCountWE', 'PreviousStepClickCountWE', 'deletedApp', 'ruleScoreMP', 'ruleScoreDS', 'ruleScoreSIMP', 'ruleScoreMT', 'ruleScoreADD', 'ruleScoreCONJ', 'ruleScoreHS', 'ruleScoreCD', 'ruleScoreDN', 'ruleScoreDEM', 'ruleScoreIMPL', 'ruleScoreCONTRA', 'ruleScoreEQUIV', 'ruleScoreCOM', 'ruleScoreASSOC', 'ruleScoreDIST', 'ruleScoreABS', 'ruleScoreEXP', 'ruleScoreTAUT', 'cumul_Interaction', 'cumul_hintCount', 'cumul_TotalTime', 'cumul_TotalPSTime', 'cumul_TotalWETime', 'cumul_avgstepTime', 'cumul_avgstepTimeWE', 'cumul_avgstepTimePS', 'cumul_symbolicRepresentationCount', 'cumul_englishSymbolicSwitchCount', 'cumul_difficultProblemCountSolved', 'cumul_difficultProblemCountWE', 'cumul_easyProblemCountSolved', 'cumul_easyProblemCountWE', 'cumul_probAlternate', 'cumul_easyProbAlternate', 'cumul_RuleTypesCount', 'cumul_UseCount', 'cumul_PrepCount', 'cumul_MorphCount', 'cumul_OptionalCount', 'cumul_probIndexinLevel', 'cumul_InterfaceErrorCount', 'cumul_RightApp', 'cumul_WrongApp', 'cumul_WrongSemanticsApp', 'cumul_WrongSyntaxApp', 'cumul_PrightAppRatio', 'cumul_RrightAppRatio', 'cumul_F1Score', 'cumul_FDActionCount', 'cumul_BDActionCount', 'cumul_DirectProofActionCount', 'cumul_InDirectProofActionCount', 'cumul_actionCount', 'cumul_UseWindowInfo', 'cumul_NonPSelements', 'cumul_AppCount', 'cumul_AppRatio', 'cumul_hintRatio', 'cumul_BlankRatio', 'cumul_HoverHintCount', 'cumul_SystemInfoHintCount', 'cumul_NextStepClickCountWE', 'cumul_PreviousStepClickCountWE', 'cumul_deletedApp', 'CurrPro_NumProbRule', 'CurrPro_avgProbTime', 'CurrPro_avgProbTimePS', 'CurrPro_avgProbTimeDeviationPS', 'CurrPro_avgProbTimeWE', 'CurrPro_avgProbTimeDeviationWE', 'CurrPro_medianProbTime']
@@ -13,13 +25,68 @@ yfinal = y.ravel()
 
 knn = KNeighborsClassifier(n_neighbors=4)
 
-sfs1 = SFS(knn, 
-           k_features=8, 
-           forward=True, 
-           floating=False, 
-           verbose=2,
-           scoring='accuracy',
-           cv=0)
+# Sequential Forward Selection
+def callSFS():
+	sfs1 = SFS(knn, 
+	           k_features=8, 
+	           forward=True, 
+	           floating=False, 
+	           verbose=2,
+	           scoring='accuracy',
+	           cv=0)
 
-sfs1 = sfs1.fit(X, yfinal)
-print sfs1.subsets_
+	sfs1 = sfs1.fit(X, yfinal)
+	print sfs1.subsets_
+
+# Sequential Backward Selection
+def callSBS():
+	sbs = SFS(knn, 
+	          k_features=8, 
+	          forward=False, 
+	          floating=False, 
+	          scoring='accuracy',
+	          cv=4,
+	          n_jobs=-1)
+
+	sbs = sbs.fit(X, yfinal)
+	print sbs.subsets_
+
+'''print('\nSequential Backward Selection (k=3):')
+print(sbs.k_feature_idx_)
+print('CV Score:')
+print(sbs.k_score_)'''
+
+# Sequential Floating Forward Selection
+def callSFFS():
+	sffs = SFS(knn, 
+	           k_features=8, 
+	           forward=True, 
+	           floating=True, 
+	           scoring='accuracy',
+	           cv=4,
+	           n_jobs=-1)
+
+	sffs = sffs.fit(X, yfinal)
+	print sffs.subsets_
+
+# Sequential Floating Backward Selection
+def callSFBS():
+	sfbs = SFS(knn, 
+	           k_features=8, 
+	           forward=False, 
+	           floating=True, 
+	           scoring='accuracy',
+	           cv=4,
+	           n_jobs=-1)
+	
+	sfbs = sfbs.fit(X, yfinal)
+	print sfbs.subsets_
+
+if function == 'SFS':
+	callSFS()
+elif function =='SBS':
+	callSBS()
+elif function == 'SFFS':
+	callSFFS()
+elif function == 'SBFS':
+	callSBFS()
--- discretize.py
+++ discretize.py
@@ -4,15 +4,18 @@ import pandas as pd
 import numpy as np
 import csv
 from scipy.stats import binned_statistic
+import os
 
 url = "MDP_Original_data2.csv";
 names = ['student', 'currProb', 'course', 'session', 'priorTutorAction', 'reward', 'Interaction', 'hintCount', 'TotalTime', 'TotalPSTime', 'TotalWETime', 'avgstepTime', 'avgstepTimePS', 'stepTimeDeviation', 'symbolicRepresentationCount', 'englishSymbolicSwitchCount', 'Level', 'probDiff', 'difficultProblemCountSolved', 'difficultProblemCountWE', 'easyProblemCountSolved', 'easyProblemCountWE', 'probAlternate', 'easyProbAlternate', 'RuleTypesCount', 'UseCount', 'PrepCount', 'MorphCount', 'OptionalCount', 'NewLevel', 'SolvedPSInLevel', 'SeenWEinLevel', 'probIndexinLevel', 'probIndexPSinLevel', 'InterfaceErrorCount', 'RightApp', 'WrongApp', 'WrongSemanticsApp', 'WrongSyntaxApp', 'PrightAppRatio', 'RrightAppRatio', 'F1Score', 'FDActionCount', 'BDActionCount', 'DirectProofActionCount', 'InDirectProofActionCount', 'actionCount', 'UseWindowInfo', 'NonPSelements', 'AppCount', 'AppRatio', 'hintRatio', 'BlankRatio', 'HoverHintCount', 'SystemInfoHintCount', 'NextStepClickCountWE', 'PreviousStepClickCountWE', 'deletedApp', 'ruleScoreMP', 'ruleScoreDS', 'ruleScoreSIMP', 'ruleScoreMT', 'ruleScoreADD', 'ruleScoreCONJ', 'ruleScoreHS', 'ruleScoreCD', 'ruleScoreDN', 'ruleScoreDEM', 'ruleScoreIMPL', 'ruleScoreCONTRA', 'ruleScoreEQUIV', 'ruleScoreCOM', 'ruleScoreASSOC', 'ruleScoreDIST', 'ruleScoreABS', 'ruleScoreEXP', 'ruleScoreTAUT', 'cumul_Interaction', 'cumul_hintCount', 'cumul_TotalTime', 'cumul_TotalPSTime', 'cumul_TotalWETime', 'cumul_avgstepTime', 'cumul_avgstepTimeWE', 'cumul_avgstepTimePS', 'cumul_symbolicRepresentationCount', 'cumul_englishSymbolicSwitchCount', 'cumul_difficultProblemCountSolved', 'cumul_difficultProblemCountWE', 'cumul_easyProblemCountSolved', 'cumul_easyProblemCountWE', 'cumul_probAlternate', 'cumul_easyProbAlternate', 'cumul_RuleTypesCount', 'cumul_UseCount', 'cumul_PrepCount', 'cumul_MorphCount', 'cumul_OptionalCount', 'cumul_probIndexinLevel', 'cumul_InterfaceErrorCount', 'cumul_RightApp', 'cumul_WrongApp', 'cumul_WrongSemanticsApp', 'cumul_WrongSyntaxApp', 'cumul_PrightAppRatio', 'cumul_RrightAppRatio', 'cumul_F1Score', 'cumul_FDActionCount', 'cumul_BDActionCount', 'cumul_DirectProofActionCount', 'cumul_InDirectProofActionCount', 'cumul_actionCount', 'cumul_UseWindowInfo', 'cumul_NonPSelements', 'cumul_AppCount', 'cumul_AppRatio', 'cumul_hintRatio', 'cumul_BlankRatio', 'cumul_HoverHintCount', 'cumul_SystemInfoHintCount', 'cumul_NextStepClickCountWE', 'cumul_PreviousStepClickCountWE', 'cumul_deletedApp', 'CurrPro_NumProbRule', 'CurrPro_avgProbTime', 'CurrPro_avgProbTimePS', 'CurrPro_avgProbTimeDeviationPS', 'CurrPro_avgProbTimeWE', 'CurrPro_avgProbTimeDeviationWE', 'CurrPro_medianProbTime']
+#names = ['student', 'currProb', 'course', 'session', 'priorTutorAction', 'reward', 'Interaction', 'hintCount']
 dataframe = read_csv(url, names=names, dtype = object)
 array = dataframe.values
 
 X = array[1:,]
 
 def discretize_and_write(col):
+
 	Xfinal =  X[0:,col].astype(np.float)
 
 	max_value = max(Xfinal)
@@ -29,6 +32,7 @@ def discretize_and_write(col):
 
 	bin_list = [-np.inf,b1,b2,b3,max_value]
 	bin_list = sorted(list(set(bin_list)))
+	print bin_list
 	label = []
 	for i in range(len(bin_list)-1):
 		label.append(i)
@@ -42,7 +46,7 @@ def discretize_and_write(col):
 def histogram_disc(col):
 	Xfinal =  X[0:,col].astype(np.float)
 
-	hist,bin_list = np.histogram(Xfinal, bins="auto")
+	hist,bin_list = np.histogram(Xfinal, bins=4)
 	label = []
 	for i in range(len(bin_list)):
 		label.append(i)
@@ -57,29 +61,35 @@ def histogram_disc(col):
 	return b
 
 
-selected_features = [80,12,9,83,6,84,14,128]
+selected_features = [119, 120, 57, 74, 123, 12, 106, 30]
 discretized_features = []
 
 for i in selected_features:
-	discretized_features.append(histogram_disc(i))
+	discretized_features.append(discretize_and_write(i))
 
 features = []
-features.append(('feature1','feature2','feature3','feature4','feature5','feature6','feature7','feature8'))
+features.append(('student','currProb','course','session','priorTutorAction','reward','feature1','feature2','feature3','feature4','feature5','feature6','feature7','feature8'))
+#features.append(('feature1','feature2','feature3','feature4','feature5','feature6','feature7','feature8'))
+#119, 120, 57, 74, 123, 12, 106, 30
 
-'''existing_features = []
+existing_features = []
 with open('sample_output.csv', 'r') as csv_file:
     reader = csv.reader(csv_file, delimiter=',')
     for row in reader:
         existing_features.append(row)
-csv_file.close()'''
+csv_file.close()
 
 for j in range(len(discretized_features[0])):
-		features.append((discretized_features[0][j],discretized_features[1][j],discretized_features[2][j],discretized_features[3][j],discretized_features[4][j],discretized_features[5][j],discretized_features[6][j],discretized_features[7][j]))
-		
-with open('a.csv','w') as out:
-	writer = csv.writer(out, delimiter = '\t')
+		features.append((list(existing_features[j+1])+list((discretized_features[0][j],discretized_features[1][j],discretized_features[2][j],discretized_features[3][j],discretized_features[4][j],discretized_features[5][j],discretized_features[6][j],discretized_features[7][j]))))
+		#features.append(((discretized_features[0][j],discretized_features[1][j],discretized_features[2][j],discretized_features[3][j],discretized_features[4][j],discretized_features[5][j],discretized_features[6][j],discretized_features[7][j])))
+
+with open('discretized_output.csv','w') as out:
+	writer = csv.writer(out, delimiter = ',')
 	for row in features:
 		writer.writerow(row)
+out.close()
+
+os.system("python MDP_process2.py -input discretized_output.csv")
 
 '''with open('a.csv', 'wb') as out:
     writer = csv.writer(out)
